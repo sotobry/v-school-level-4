b3848e18ffc70708ea434eb0faf8b2fb40539d72
Bryann Sotomayor <bryann.sotomayor@gmail.com>
2021-11-02 11:31:27 -0700

    Finish React Router - Redirect lesson.

--- src/lessons/23_reactRouter/ReactRouter.js
+++ src/lessons/23_reactRouter/ReactRouter.js
@@ -1,45 +1,68 @@
-import { Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
+import {
+  Link, Switch, Route,
+  Redirect
+} from 'react-router-dom';
 // import About from './pages/About';
-import Header from './Header';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
+// import Header from './Header';
+// import Home from './pages/Home';
+// import Profile from './pages/Profile';
 // import Info from './pages/profile/Info';
 // import Settings from './pages/profile/Settings';
-import Footer from './Footer';
-import ServicesList from './pages/services/ServicesList';
-import ServiceDetail from './pages/services/ServiceDetail';
+// import Footer from './Footer';
+// import ServicesList from './pages/services/ServicesList';
+// import ServiceDetail from './pages/services/ServiceDetail';
 
 const ReactRouter = () => {
+  const [isLoggedIn, setIsLoggedInTo] = useState(false);
   return (
     <div>
-      <Header />
-      <Switch>
-        {/* <Route exact path='/' render={() => {
+      {/* <Header /> */}
+      {/* <Switch> */}
+      {/* <Route exact path='/' render={() => {
           return (<h1>Home page!</h1>);
         }} />
         <Route path='/about' render={() => {
           return (<h1>About page!</h1>);
         }} /> */}
 
-        {/* <Route exact path='/' component={Home} />
+      {/* <Route exact path='/' component={Home} />
         <Route path='/about' component={About} /> */}
 
-        <Route exact path='/'><Home /></Route>
-        <Route path='/profile'><Profile /></Route>
-        {/* <Route path='/profile/info'>
+      {/* <Route exact path='/'><Home /></Route> */}
+      {/* <Route path='/profile'><Profile /></Route> */}
+      {/* <Route path='/profile/info'>
           <Info />
         </Route>
         <Route path='/profile/settings'>
           <Settings />
         </Route> */}
-        <Route exact path='/services'>
-          <ServicesList />
+      {/* <Route exact path='/services'> */}
+      {/* <ServicesList /> */}
+      {/* </Route> */}
+      {/* <Route path='/services/:serviceId'> */}
+      {/* <ServiceDetail /> */}
+      {/* </Route> */}
+      {/* </Switch> */}
+      {/* <Footer /> */}
+      <Link to='/'>Home</Link>
+      <Link to='/private'>Private</Link>
+
+      <Switch>
+        <Route exact path='/'>
+          <h1>Home page, anyone is allowed here.</h1>
         </Route>
-        <Route path='/services/:serviceId'>
-          <ServiceDetail />
+        <Route path='/private'>
+          {isLoggedIn ?
+            <h1>Protected page, must be logged in to be here.</h1> :
+            <Redirect to='/login' />
+          }
+        </Route>
+        <Route path='/login'>
+          <h1>Log in page.</h1>
+          <button onClick={() => setIsLoggedInTo(true)}>Press to log in.</button>
         </Route>
       </Switch>
-      <Footer />
     </div>
   );
 };
